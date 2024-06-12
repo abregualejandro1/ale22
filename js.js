bf2133e9a074deafc9d36427207b40b548d80677
abregualejandro1 <abregu.alejandrodavid@iesconcepcion.edu.ar>
2024-06-11 22:20:37 -0300

    probando commit

---
+++ js.js
@@ -0,0 +1,114 @@
+// //   IF, ELSE IF, ELSE       PRIMERA PRUEBA.
+// let numero = 53;
+
+// if (numero < 50){
+//     alert("correcto es mayor")
+// }
+
+// else if (numero > 60){
+// alert("correcto es menor")
+// }
+
+// else if (numero > 12){
+//  alert("si esto funciona");
+// }
+
+// else {
+//     alert("lo siento ninguno coincide");
+// }
+
+
+
+//   IF, ELSE IF, ELSE       SEGUNDA PRUEBA.
+
+// let pcLucas = prompt("cuanto dinero tienes Lucas?");
+// let pcSandro = prompt("cuanto dinero tienes Sandro");
+
+// if (pcLucas > 15000 && pcLucas < 20000){
+//      alert("Lucas podes comprar una ram");
+// }
+
+// else if (pcLucas > 20000 && pcLucas < 30000){
+//     alert("Lucas podes comprar un procesador");
+// }
+
+// else if (pcLucas > 30000 && pcLucas < 40000){
+//     alert("Lucas podes comprar un combo de teclado y raton");
+// }
+
+// else if (pcLucas > 40000){
+//     alert("lucas podes comprar todo");
+// }
+
+// else {
+//     alert("lo siento lucas no te alcanza para nada")
+// }
+
+// // pcsandro
+
+// if (pcSandro > 15000 && pcSandro < 20000){
+//     alert("Sandro podes comprar una ram");
+// }
+
+// else if (pcSandro > 20000 && pcSandro < 30000){
+//    alert("Sandro podes comprar un procesador");
+// }
+
+// else if (pcSandro > 30000 && pcSandro < 40000){
+//    alert("Sandro podes comprar un combo de teclado y raton");
+// }
+
+// else if (pcSandro > 40000){
+//    alert("Sandro podes comprar todo");
+// }
+
+// else {
+//    alert("lo siento lucas no te alcanza para nada")
+// }
+
+
+
+
+
+
+
+//    MANERA DE SUMAR.
+// let numero1 = 30;
+// let numero2 = 20;
+
+// let resultado = ( numero1 + numero2);
+// alert (resultado);
+
+
+//  OTRA MANERA.
+
+// function suma (a, b){
+// return a + b;
+// }
+
+// let sumando = suma(30, 30);
+// alert(sumando);
+
+
+//aca le aumentamos de a uno con ++ tambien se le resta con --   .
+// let numero1 = 2;
+// let numero2 = 3;
+// numero1++
+// numero1++
+// numero1++
+// let resultado = numero1++;
+// alert(resultado)
+
+
+//   USANDO BACTICKS. ``        PODEMOS ESCRIBIR HTML EN ELLOS.
+// let nombre = "Alejandro David";
+
+let frase = ` Hola mi nombre es ${nombre} 
+<h1>hola como estas</h1>
+y estoy caminando`;
+
+document.write(frase);
+
+
+
+OPERADORES DE COMPARACION. 
